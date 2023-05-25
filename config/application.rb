6a95d98897abff0d622c987f553f3e271ee2c65b
Diep <dieptc2003@gmail.com>
2023-05-25 21:10:04 +0700

    Config for blocked hosting

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module Dental
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << "dental-production-15c1.up.railway.app"
   end
 end
